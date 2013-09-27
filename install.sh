3b7b6f2c48642b637b7a49ffb3fbe457159a1840
David Gamba <davidgamba@gmail.com>
2013-09-26 20:00:07 -0600

    Update install to install bin as an option

--- install.sh
+++ install.sh
@@ -1,5 +1,46 @@
 #!/bin/bash
 
+function usage()
+{
+  option=$1
+  if [[ -n "$option" ]]; then
+    cat <<EOL
+Invalid argument: '$option'
+
+EOL
+  fi
+  cat <<EOL
+    install -h|--help
+
+    install [--bin]
+EOL
+}
+
+function main()
+{
+  while test $# -gt 0 ; do
+    case "$1" in
+      -h|--help)
+        usage
+        exit 1
+      ;;
+      --bin)
+        shift
+        echo installing bin...
+        install_bin
+        exit 0
+      ;;
+      -*)
+        usage $1
+        exit 1
+      ;;
+    esac
+  done
+  echo installing dotrc...
+  install_dotrc
+  exit 0
+}
+
 function create_link_for()
 {
   link=$1
@@ -23,18 +64,41 @@ function create_link_for()
   fi
 }
 
-create_link_for 'bashrc'
-create_link_for 'vimrc'
-create_link_for 'vim'
-create_link_for 'screenrc'
-create_link_for 'perltidyrc'
-create_link_for 'inputrc'
+function install_dotrc()
+{
+  create_link_for 'bashrc'
+  create_link_for 'vimrc'
+  create_link_for 'vim'
+  create_link_for 'screenrc'
+  create_link_for 'perltidyrc'
+  create_link_for 'inputrc'
 
-cp -n $HOME/dotrc/gitconfig $HOME/.gitconfig
+  cp -n $HOME/dotrc/gitconfig $HOME/.gitconfig
 
-if [[ ! -d $HOME/.vim/bundle/vundle ]]; then
-  git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
-  vim +BundleInstall +qall
-fi
+  if [[ ! -d $HOME/.vim/bundle/vundle ]]; then
+    git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+    vim +BundleInstall +qall
+  fi
+  echo done installing dotrc!
+}
 
-echo done!
+function install_bin()
+{
+  if [[ ! -d $HOME/bin ]]; then
+    set -x
+    git clone https://github.com/DavidGamba/bin.git $HOME/bin
+    set +x
+  fi
+  if [[ ! -d $HOME/bin/external ]]; then
+    set -x
+    mkdir $HOME/bin/external
+    git clone https://github.com/DavidGamba/grepp.git $HOME/bin/external/grepp
+    ln -s $HOME/bin/external/grepp/grepp $HOME/bin/grepp
+    git clone https://github.com/DavidGamba/todo.git $HOME/bin/external/todo
+    ln -s $HOME/bin/external/todo/todo $HOME/bin/todo
+    set +x
+  fi
+  echo done installing bin!
+}
+
+main "$@"
