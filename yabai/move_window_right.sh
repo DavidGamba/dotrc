6c4d59bba41f6e37b7f88ae16fcc17ec775848e9
David Gamba <davidgamba@gmail.com>
2021-10-02 14:46:05 -0600

    Add yabai config

---
+++ yabai/move_window_right.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Taken from:
+# https://www.notion.so/Yabai-8da3b829872d432fac43181b7ff628fc
+
+window_id=$(yabai -m query --windows --window | jq -re '.id')
+echo "found window $window_id"
+
+yabai -m window --display next || yabai -m window --display last
+
+yabai -m window --focus $window_id
