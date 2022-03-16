2fb3c8f595b6de22add7c81b338b71c748fb4e1e
David Gamba <davidgamba@gmail.com>
2022-03-16 08:14:51 -0600

    Add igrep to tools

--- main.go
+++ main.go
@@ -189,6 +189,7 @@ func DevDeps(ctx context.Context, opt *getoptions.GetOpt, args []string) error {
 	cg.cmd("cargo install tealdeer")
 	cg.cmd("cargo install code-minimap")
 	cg.cmd("cargo install fd-find")
+	cg.cmd("cargo install igrep")
 
 	_ = run.CMD(strings.Split("git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf", " ")...).Log().PrintErr().Stdin().Run()
 	os.Chdir(filepath.Join(os.Getenv("HOME"), ".fzf"))
