6d78b7d192ffa200462450dbdd576df623969540
David Gamba <davidgamba@gmail.com>
2026-09-08 14:19:13 -0600

    Update PS1

--- shell_func/ps1.bash
+++ shell_func/ps1.bash
@@ -1,14 +1,14 @@
 function git_repo {
-    PROMT_CHAR='$'
-    GIT_BRANCH=`git rev-parse --abbrev-ref HEAD 2>/dev/null` &&\
-        GIT_BRANCH=" $GIT_BRANCH" &&\
-        PROMT_CHAR='$'
+	PROMT_CHAR='$'
+	GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null) &&
+		GIT_BRANCH=" $GIT_BRANCH" &&
+		PROMT_CHAR='$'
 }
 
 function smiley {
-    [ $RET -eq 0 ] && echo -ne "👌"
-    [ $RET -ne 0 ] && echo -ne "⛔ 🙄 ⛔"
-    # [ $RET -ne 0 ] && echo -ne "\e[31;1m:("
+	[ $RET -eq 0 ] && echo -ne "👌"
+	[ $RET -ne 0 ] && echo -ne "⛔ 🙄 ⛔"
+	# [ $RET -ne 0 ] && echo -ne "\e[31;1m:("
 }
 
 function k8s_ps1 {
@@ -19,60 +19,64 @@ function kubie_shell {
 	[[ -n $KUBIE_ACTIVE ]] && echo -ne " 🐱 "
 }
 
+function aws_shell {
+	[[ -n $SAML2AWS_PROFILE ]] && echo -ne " 󰅠 $SAML2AWS_PROFILE"
+}
+
 function ps1 {
-    local BLACK="30"
-    local RED="31"
-    local GREEN="32"
-    local YELLOW="33"
-    local BLUE="34"
-    local MAGENTA="35"
-    local CYAN="36"
-    local WHITE="37"
-    local DEFAULT="39"
+	local BLACK="30"
+	local RED="31"
+	local GREEN="32"
+	local YELLOW="33"
+	local BLUE="34"
+	local MAGENTA="35"
+	local CYAN="36"
+	local WHITE="37"
+	local DEFAULT="39"
 
-    # Background
-    local B_BLACK="40"
-    local B_RED="41"
-    local B_GREEN="42"
-    local B_YELLOW="43"
-    local B_BLUE="44"
-    local B_MAGENTA="45"
-    local B_CYAN="46"
-    local B_WHITE="47"
-    local B_DEFAULT="49"
+	# Background
+	local B_BLACK="40"
+	local B_RED="41"
+	local B_GREEN="42"
+	local B_YELLOW="43"
+	local B_BLUE="44"
+	local B_MAGENTA="45"
+	local B_CYAN="46"
+	local B_WHITE="47"
+	local B_DEFAULT="49"
 
-    local RESET="0"
-    local BOLD="1"
-    local FAINT="2"
-    local ITALIC="3"
-    local NORMAL="22"
-    local UNDERLINE="4"
-    local BLINK_SLOW="5"
-    local BLINK_FAST="6"
-    local REVERSE="7"
+	local RESET="0"
+	local BOLD="1"
+	local FAINT="2"
+	local ITALIC="3"
+	local NORMAL="22"
+	local UNDERLINE="4"
+	local BLINK_SLOW="5"
+	local BLINK_FAST="6"
+	local REVERSE="7"
 
-    # insert \[ and \] around the ANSI escapes so that the shell knows not
-    # to include them in the line wrapping calculation
-    local CB="\[\e[0m\]\[\e[" # Color Begin
-    local CE="m\]"   # Color End
-    local TERM_RESET="\[\017\]"
+	# insert \[ and \] around the ANSI escapes so that the shell knows not
+	# to include them in the line wrapping calculation
+	local CB="\[\e[0m\]\[\e[" # Color Begin
+	local CE="m\]"            # Color End
+	local TERM_RESET="\[\017\]"
 
-    local PS1_START_MARK="\[\e]133;A\a\]"
-    local PS2_START_MARK="\[\e]133;A;k=s\a\]"
-    local COMMAND_START_MARK="\[\e]133;C\a\]"
+	local PS1_START_MARK="\[\e]133;A\a\]"
+	local PS2_START_MARK="\[\e]133;A;k=s\a\]"
+	local COMMAND_START_MARK="\[\e]133;C\a\]"
 
-    HOST_COLOR=${CYAN}
-    if [[ $(uname -a) =~ "amzn2" ]]; then
-        HOST_COLOR=${MAGENTA}
-    elif [[ $(uname -a) =~ "Ubuntu" ]]; then
-        HOST_COLOR=${MAGENTA}
-    fi
+	HOST_COLOR=${CYAN}
+	if [[ $(uname -a) =~ "amzn2" ]]; then
+		HOST_COLOR=${MAGENTA}
+	elif [[ $(uname -a) =~ "Ubuntu" ]]; then
+		HOST_COLOR=${MAGENTA}
+	fi
 
-    local TERM_TITLE="\[\033]2; \h \w\007\]"
-    local FIRST_LINE="${CB}${B_DEFAULT};${HOST_COLOR};${BOLD}${CE}\h ${CB}${FAINT};${HOST_COLOR}${CE}\D{%F %T} ${CB}${CYAN}${CE}\w${CB}${DEFAULT};${RESET}${CE} \$(smiley)${CB}${HOST_COLOR};${NORMAL}${CE} \${GIT_BRANCH} ${CB}${GREEN};${NORMAL}${CE}\${K8S_CONTEXT} \$(kubie_shell)"
-    local SECOND_LINE="\n${CB}${B_DEFAULT};${BLUE};${BOLD}${CE}\${PROMT_CHAR} "
-    local REGULAR_TEXT="${CB}${DEFAULT};${RESET}${CE}"
-    PS1=${TERM_RESET}${PS1_START_MARK}${TERM_TITLE}${FIRST_LINE}${SECOND_LINE}${REGULAR_TEXT}${COMMAND_START_MARK}
+	local TERM_TITLE="\[\033]2; \h \w\007\]"
+	local FIRST_LINE="${CB}${B_DEFAULT};${HOST_COLOR};${BOLD}${CE}\h ${CB}${FAINT};${HOST_COLOR}${CE}\D{%F %T} ${CB}${CYAN}${CE}\w${CB}${DEFAULT};${RESET}${CE} \$(smiley)${CB}${HOST_COLOR};${NORMAL}${CE} \${GIT_BRANCH} ${CB}${GREEN};${NORMAL}${CE}\${K8S_CONTEXT} \$(kubie_shell) \$(aws_shell)"
+	local SECOND_LINE="\n${CB}${B_DEFAULT};${BLUE};${BOLD}${CE}\${PROMT_CHAR} "
+	local REGULAR_TEXT="${CB}${DEFAULT};${RESET}${CE}"
+	PS1=${TERM_RESET}${PS1_START_MARK}${TERM_TITLE}${FIRST_LINE}${SECOND_LINE}${REGULAR_TEXT}${COMMAND_START_MARK}
 }
 
 PROMPT_COMMAND='RET=$?; stty sane; tput rmacs; history -a; git_repo; k8s_ps1'
--- shell_func/ps1.zsh
+++ shell_func/ps1.zsh
@@ -21,53 +21,53 @@ function kubie_shell {
 }
 
 function aws_shell {
-	[[ -n $AWS_OKTA_PROFILE ]] && echo -ne " %F{#FF9900}󰅠 %F{#d37e00}$AWS_OKTA_PROFILE"
+	[[ -n $SAML2AWS_PROFILE ]] && echo -ne " %F{#FF9900}󰅠 %F{#d37e00}$SAML2AWS_PROFILE"
 }
 
 function ps1 {
-    local BLACK="30"
-    local RED="31"
-    local GREEN="32"
-    local YELLOW="33"
-    local BLUE="34"
-    local MAGENTA="35"
-    local CYAN="36"
-    local WHITE="37"
-    local DEFAULT="39"
+	local BLACK="30"
+	local RED="31"
+	local GREEN="32"
+	local YELLOW="33"
+	local BLUE="34"
+	local MAGENTA="35"
+	local CYAN="36"
+	local WHITE="37"
+	local DEFAULT="39"
 
-    # Background
-    local B_BLACK="40"
-    local B_RED="41"
-    local B_GREEN="42"
-    local B_YELLOW="43"
-    local B_BLUE="44"
-    local B_MAGENTA="45"
-    local B_CYAN="46"
-    local B_WHITE="47"
-    local B_DEFAULT="49"
+	# Background
+	local B_BLACK="40"
+	local B_RED="41"
+	local B_GREEN="42"
+	local B_YELLOW="43"
+	local B_BLUE="44"
+	local B_MAGENTA="45"
+	local B_CYAN="46"
+	local B_WHITE="47"
+	local B_DEFAULT="49"
 
-    local RESET="0"
-    local BOLD="1"
-    local FAINT="2"
-    local ITALIC="3"
-    local NORMAL="22"
-    local UNDERLINE="4"
-    local BLINK_SLOW="5"
-    local BLINK_FAST="6"
-    local REVERSE="7"
+	local RESET="0"
+	local BOLD="1"
+	local FAINT="2"
+	local ITALIC="3"
+	local NORMAL="22"
+	local UNDERLINE="4"
+	local BLINK_SLOW="5"
+	local BLINK_FAST="6"
+	local REVERSE="7"
 
-    HOST_COLOR=${CYAN}
-    if [[ $(uname -a) =~ "amzn2" ]]; then
-        HOST_COLOR=${MAGENTA}
-    elif [[ $(uname -a) =~ "Ubuntu" ]]; then
-        HOST_COLOR=${MAGENTA}
-    fi
-		builtin local ps1_start_mark=$'%{\e]133;A\a%}'
-		builtin local ps2_start_mark=$'%{\e]133;A;k=s\a%}'
-		builtin local cmd_start_mark=$'%{\e]133;C\a%}'
+	HOST_COLOR=${CYAN}
+	if [[ $(uname -a) =~ "amzn2" ]]; then
+			HOST_COLOR=${MAGENTA}
+	elif [[ $(uname -a) =~ "Ubuntu" ]]; then
+			HOST_COLOR=${MAGENTA}
+	fi
+	builtin local ps1_start_mark=$'%{\e]133;A\a%}'
+	builtin local ps2_start_mark=$'%{\e]133;A;k=s\a%}'
+	builtin local cmd_start_mark=$'%{\e]133;C\a%}'
 
-		PS1=${ps1_start_mark}$'%F{36}%m %F{2;36}%D %* %F{#44c9c9}%~%f %(?.👌.⛔ 🙄 ⛔)$(ps1_git_branch)$(k8s_ps1)$(kubie_shell)$(aws_shell)
-'${ps2_start_mark}'%F{#8360d1}%(!.#.$)%f '${cmd_start_mark}
+	PS1=${ps1_start_mark}$'%F{36}%m %F{2;36}%D %* %F{#44c9c9}%~%f %(?.👌.⛔ 🙄 %? ⛔)$(ps1_git_branch)$(k8s_ps1)$(kubie_shell)$(aws_shell)
+	'${ps2_start_mark}'%F{#8360d1}%(!.#.$)%f '${cmd_start_mark}
 }
 
 # precmd () { RET=$?; stty sane; tput rmacs; history -a; git_repo; k8s_ps1}
